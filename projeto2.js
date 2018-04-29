b2202d3a010fc4f75ef2540905eb23efa3634453
MAPtsiw <38010085+MAPtsiw@users.noreply.github.com>
2018-04-30 00:52:07 +0100

    Inicio do javaScript

--- projeto2.js
+++ projeto2.js
@@ -1,7 +1,181 @@
+//Variáveis Globais:
+let utilizadores = []
+
+//Vai servir para saber se há alguém com sessão iniciada ou não
+let logged = false;
+
+//Vai simplificar saber qual é o indice no array utilizadores do utilizador loggado
+let indexUtilizador = 0;
+
+//------------------------------------------
+
+
 window.onload = function () {
 
+    //Registar um utilizador
+    let formRegistar = document.getElementById('FormRegisto')
+    formRegistar.addEventListener('submit', function () {
+        let nome = document.getElementById('Nome').value
+        let password = document.getElementById('PassRegisto').value
+        let confPass = document.getElementById('ConfPassRegisto').value
+        let eMail = document.getElementById('Mail').value
+
+        //Confirmar Pass's
+        if (password == confPass) {
+            //Criar Utilizador
+            let novoUtilizador = new Utilizador(nome, password, eMail)
+
+            //Mete-lo no array
+            utilizadores.push(novoUtilizador)
+
+            //Devem faltar merdas
+        }
+        else {
+            let msgErro = document.getElementById('MsgErroRegistar') //Dá para usar outra vez o nome da variável
+
+            msgErro.innerHTML = "As passowrds têm que ser iguais"
+        }
+    })
+
+
+    //Fazer Login
+    let formLogin = document.getElementById('FormLogin')
+    formLogin.addEventListener('submit', function () {
+        let ok = false
+
+        //Vai servir para personalizar a mensagem de erro.
+        let erroMail = true;
+        let erroPass = true;
+        let erro = ""
+
+        let mail = document.getElementById('mailLogin').value
+        let pass = document.getElementById('passLogin').value
+
+        for (let i = 0; i < utilizadores.length; i++) {
+            if (utilizadores[i].mail == mail) {
+
+                erroMail = false //Ou seja, não há erros
+
+                if (utilizadores[i].password == pass) {
+                    ok = true
+                    erroPass = false;
+                    indexUtilizador = i; //Simplificar a vida
+                }
+            }
+        }
+
+        if (ok == true) {
+            //Dizer que o utilizador está com sessão iniciada
+            logged = true; //Vai servir para várias merdas a seguir
+
+            //Mostrar o botão de log-off e esconder o de log-in
+            let btnLogin = document.getElementById('Login')
+            btnLogin.style.display = "none"
+
+            let btnLogoff = document.getElementById('Logoff')
+            btnLogoff.style.display = "inline-block"
+
+            //Botão de registar disabled, não me parece a melhor opção, mas de outra maneira daria trabalho a mais para agora
+            let btnRegistar = document.getElementById('Registar')
+            btnRegistar.disabled = true
+        }
+        else {
+            let msgErro = document.getElementById('MsgErroLogin')
+            msgErro.innerHTML = mensagemErro(erroMail, erroPass) //Função que personaliza a mensagem de erro
+        }
+    })
+
+    let btnLogoff = document.getElementById('Logoff')
+    btnLogoff.addEventListener('click', function () {
+
+        //Ativar o botão de registar
+        let btnRegistar = document.getElementById('Registar')
+        btnRegistar.disabled = false
+
+
+        //Esconder o botão de log-off e mostrar o de log-in
+        let btnLogoff = document.getElementById('Logoff')
+        btnLogoff.style.display = "none"
+
+
+        let btnLogin = document.getElementById('Login')
+        btnLogin.style.display = "inline-block"
+
+        indexUtilizador = 0
+
+        logged = false; //Em principio vai ser esta variavel que vai dizer o que é que se mostra ou não nas páginas
+
+    })
 }
 
+//---------------------------------------------- Classes ----------------------------------------------------------------------------------------
+//Isto mal possa vai para um ficheiro à parte
+class Utilizador {
+    constructor(nome, pass, mail) { //Por agora vai ficar assim, não é dificil de acrescentar merdas
+        this.nome = nome
+        this.password = pass
+        this.mail = mail
+
+        this._id = Utilizador.getLastId() + 1
+    }
+
+    get nome() {
+        return this._nome
+    }
+
+    set nome(valor) {
+        this._nome = valor
+    }
+
+    get password() {
+        return this._passowrd
+    }
+
+    set password(valor) {
+        this._password = valor
+    }
+
+    get mail() {
+        return this._mail
+    }
+
+    set mail(valor) {
+        this._mail = valor
+    }
+
+    // Get the last ID
+    static getLastId() {
+        let lastId = 0
+        if (utilizadores.length > 0) {
+            lastId = utilizadores[utilizadores.length - 1].id
+            console.log('O lastId do utilizador é = ' + lastId)
+        }
+
+        return lastId
+    }
+}
+
+
+
+//---------------------------------------------- Funções ----------------------------------------------------------------------------------------
+
+//Personalizar mensagem de erro
+function mensagemErro(email, passi) {
+    let mensagem = ""
+
+    if (email) { //Ou seja o email não existe
+        mensagem = "O mail que introduziu não existe"
+    }
+    else if (pass) { //Se o mail existe mas a pass é true, então é porque o mail e a pass não correspondem
+        mensagem = "A password está errada"
+    }
+
+    return mensagem
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------
+
+//Por este tipo de merdas num outro ficheiro secalhar
 //Tem que estar fora do window.onload
 function Mapa() {
     let location = new google.maps.LatLng(41.366858, -8.738309);
@@ -13,30 +187,30 @@ function Mapa() {
     });
 
     // Ponto no mapa....
-    let mark =  new google.maps.Marker({
+    let mark = new google.maps.Marker({
         position: location,
         map: map
     });
-    
+
 
 
     //var map = new google.maps.Map(document.getElementById("mapa"), mapProp);
 }
 
 //CArrosel
-$('.carousel[data-type="multi"] .item').each(function() {
-	var next = $(this).next();
-	if (!next.length) {
-		next = $(this).siblings(':first');
-	}
-	next.children(':first-child').clone().appendTo($(this));
+$('.carousel[data-type="multi"] .item').each(function () {
+    var next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
 
-	for (var i = 0; i < 2; i++) {
-		next = next.next();
-		if (!next.length) {
-			next = $(this).siblings(':first');
-		}
+    for (var i = 0; i < 2; i++) {
+        next = next.next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
 
-		next.children(':first-child').clone().appendTo($(this));
-	}
+        next.children(':first-child').clone().appendTo($(this));
+    }
 });
